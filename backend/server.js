7503f26b5fca3e26fb651363e0e82a964ab769a5
Solares10 <solaresmichelle22@gmail.com>
2026-03-28 18:55:42 -0700

    Added to server.js-MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose"); // ADD THIS
 
 dotenv.config();
 
@@ -9,8 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//  CONNECT TO MONGODB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB error:", err));
+
 app.get("/", (req, res) => {
   res.send("SkillSwap API running");
+});
+
+// TEST ROUTE
+app.get("/test-db", async (req, res) => {
+  try {
+    await mongoose.connection.db.admin().ping();
+    res.send("MongoDB is working");
+  } catch (error) {
+    res.status(500).send("MongoDB is not working");
+  }
 });
 
 const PORT = process.env.PORT || 5000;
